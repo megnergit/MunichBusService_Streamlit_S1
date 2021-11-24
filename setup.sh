30a292640372d8f158f80942729123746953a540
Meg <miwa.egner@gmail.com>
2021-11-24 08:27:29 +0100

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
-mkdir -p ~/.streamlit/
+mkdir -p .streamlit/
+
+echo "\
+[general]\n\
+email = \"miwa.egner@gmail.com\"\n\
+" > .streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" >> ~/.streamlit/config.toml
+
+[theme]\n\
+base = \"light\"\n\
+\n\
+" > .streamlit/config.toml
